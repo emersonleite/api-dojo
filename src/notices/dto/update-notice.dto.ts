ea0c893a69721bf6703b78970ff65914b70fc603
Emerson Leite <emerson.bl@gmail.com>
2026-01-12 14:12:08 -0300

    refactor: replace PartialType with OmitType in UpdateNoticeDto for improved type safety

--- src/notices/dto/update-notice.dto.ts
+++ src/notices/dto/update-notice.dto.ts
@@ -1,4 +1,7 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateNoticeDto } from './create-notice.dto';
+import { OmitType } from "@nestjs/mapped-types";
+import { CreateNoticeDto } from "./create-notice.dto";
 
-export class UpdateNoticeDto extends PartialType(CreateNoticeDto) {}
+// OmitType: Cria um novo tipo removendo propriedades específicas de outro tipo
+// Aqui, UpdateNoticeDto herda todos os campos de CreateNoticeDto EXCETO "createdBy"
+// Útil para reutilizar validações e tipos, evitando duplicação de código
+export class UpdateNoticeDto extends OmitType(CreateNoticeDto, ["createdBy"]) {}
