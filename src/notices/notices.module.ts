3dcdecb86d2916736f3f3cec090c6d1cf6ff3c4d
Emerson Leite <emerson.bl@gmail.com>
2026-02-02 15:26:53 -0300

    refactor: use forwardRef for circular dependency resolution in NoticesModule

--- src/notices/notices.module.ts
+++ src/notices/notices.module.ts
@@ -1,12 +1,15 @@
-import { Module } from '@nestjs/common';
-import { NoticesService } from './notices.service';
-import { NoticesController } from './notices.controller';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Notice } from './entities/notice.entity';
-import { ResidentsModule } from 'src/residents/residents.module';
+import { forwardRef, Module } from "@nestjs/common";
+import { NoticesService } from "./notices.service";
+import { NoticesController } from "./notices.controller";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Notice } from "./entities/notice.entity";
+import { ResidentsModule } from "src/residents/residents.module";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Notice]), ResidentsModule],
+  imports: [
+    TypeOrmModule.forFeature([Notice]),
+    forwardRef(() => ResidentsModule),
+  ],
   controllers: [NoticesController],
   providers: [NoticesService],
 })
